68c67f89a33025f3a2ef0dc22be58673467916ad
mivchal <mivchal@hotmail.com>
2021-08-19 23:08:14 +0200

    Initial commit

---
+++ src/main/java/pb/edu/pl/krysiukm/learnit/LearnItApplication.java
@@ -0,0 +1,13 @@
+package pb.edu.pl.krysiukm.learnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pb/edu/pl/krysiukm/learnit/LearnItApplicationTests.java
@@ -0,0 +1,13 @@
+package pb.edu.pl.krysiukm.learnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
