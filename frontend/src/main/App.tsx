70c124280a2cbd8b513039e99c6cf61e156309fd
mivchal <mivchal@hotmail.com>
2021-08-28 10:52:59 +0200

    react router

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import HomePage from "./main/HomePage";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>LearnIT</h1>
-    </div>
-  );
+const App = () => {
+
+
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/">
+                        <HomePage/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/main/main/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from "@material-ui/core";
+
+const  HomePage = () => {
+    return (
+        <Button>Hello!</Button>
+    );
+}
+
+export default HomePage;
