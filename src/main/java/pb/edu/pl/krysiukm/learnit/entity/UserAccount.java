674addba1ac2a82eeec0dc02e3bb8ad2ad74cebb
mivchal <mivchal@gmail.com>
2021-11-19 22:29:43 +0100

    user id to email

--- src/main/java/pb/edu/pl/krysiukm/learnit/entity/UserAccount.java
+++ src/main/java/pb/edu/pl/krysiukm/learnit/entity/UserAccount.java
@@ -2,6 +2,7 @@ package pb.edu.pl.krysiukm.learnit.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -13,21 +14,19 @@ import java.util.Collection;
 public class UserAccount {
 
     @Id
-    @Column(unique = true)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String email;
 
     private String firstName;
-
     private String lastName;
-
-    private String email;
 
     @Column(length = 60)
     private String password;
 
+    @ColumnDefault("0")
+    private long points;
+
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "USERS_ROLES", joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "ROLE_NAME", referencedColumnName = "NAME"))
+    @JoinTable(name = "USERS_ROLES", joinColumns = @JoinColumn(name = "USER_EMAIL", referencedColumnName = "EMAIL"), inverseJoinColumns = @JoinColumn(name = "ROLE_NAME", referencedColumnName = "NAME"))
     private Collection<Role> roles;
 
 }
