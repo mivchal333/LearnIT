2bac04a398907afbde21528d96fc27bcc1fe8100
mivchal <mivchal@gmail.com>
2021-11-19 22:00:00 +0100

    switch to user from security context

--- src/main/java/pb/edu/pl/krysiukm/learnit/controller/AttemptController.java
+++ src/main/java/pb/edu/pl/krysiukm/learnit/controller/AttemptController.java
@@ -2,11 +2,14 @@ package pb.edu.pl.krysiukm.learnit.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 import pb.edu.pl.krysiukm.learnit.entity.UserAccount;
 import pb.edu.pl.krysiukm.learnit.entity.UserAttempt;
-import pb.edu.pl.krysiukm.learnit.repository.UserRepository;
 import pb.edu.pl.krysiukm.learnit.service.UserAttemptService;
+import pb.edu.pl.krysiukm.learnit.service.UserService;
+import pb.edu.pl.krysiukm.learnit.service.exception.NotFoundException;
 
 import java.util.List;
 
@@ -15,20 +18,26 @@ import java.util.List;
 @RequestMapping("/attempt")
 public class AttemptController {
     private final UserAttemptService userAttemptService;
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserAttempt> startAttempt(@RequestParam Long technologyId) {
-        // TODO: get user from security context
-        UserAccount userAccount = userRepository.findAll().iterator().next();
+    public ResponseEntity<UserAttempt> startAttempt(@RequestParam Long technologyId,
+                                                    @AuthenticationPrincipal User user) {
+        String email = user.getUsername();
+        UserAccount userAccount = userService.getUserAccount(email)
+                .orElseThrow(() -> new NotFoundException("User with email " + email + " not found."));
+
         UserAttempt attempt = userAttemptService.startQuizAttempt(userAccount, technologyId);
         return ResponseEntity.ok(attempt);
     }
 
     @GetMapping
-    public ResponseEntity<List<UserAttempt>> getUserActiveAttempts() {
-        // TODO: get user from security context
-        UserAccount userAccount = userRepository.findAll().iterator().next();
+    public ResponseEntity<List<UserAttempt>> getUserActiveAttempts(
+            @AuthenticationPrincipal User user) {
+        String email = user.getUsername();
+        UserAccount userAccount = userService.getUserAccount(email)
+                .orElseThrow(() -> new NotFoundException("User with email " + email + " not found."));
+
 
         List<UserAttempt> attempts = userAttemptService.getUserAttempts(userAccount);
 
--- src/main/java/pb/edu/pl/krysiukm/learnit/controller/UserHistoryController.java
+++ src/main/java/pb/edu/pl/krysiukm/learnit/controller/UserHistoryController.java
@@ -3,13 +3,15 @@ package pb.edu.pl.krysiukm.learnit.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 import pb.edu.pl.krysiukm.learnit.dto.UserAttemptDto;
 import pb.edu.pl.krysiukm.learnit.dto.UserAttemptMapper;
 import pb.edu.pl.krysiukm.learnit.entity.UserAccount;
 import pb.edu.pl.krysiukm.learnit.entity.UserAttempt;
-import pb.edu.pl.krysiukm.learnit.repository.UserRepository;
 import pb.edu.pl.krysiukm.learnit.service.UserAttemptService;
+import pb.edu.pl.krysiukm.learnit.service.UserService;
 import pb.edu.pl.krysiukm.learnit.service.exception.NotFoundException;
 
 import java.util.List;
@@ -21,12 +23,15 @@ import java.util.stream.Collectors;
 @RequestMapping("/history")
 public class UserHistoryController {
     private final UserAttemptService userAttemptService;
-    private final UserRepository userRepository;
     private final UserAttemptMapper userAttemptMapper;
+    private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<?> getUserHistoryByTechnology(@RequestParam Long technologyId) {
-        UserAccount userAccount = userRepository.findAll().iterator().next();
+    public ResponseEntity<?> getUserHistoryByTechnology(@RequestParam Long technologyId,
+                                                        @AuthenticationPrincipal User user) {
+        String email = user.getUsername();
+        UserAccount userAccount = userService.getUserAccount(email)
+                .orElseThrow(() -> new NotFoundException("User with email " + email + " not found."));
 
         List<UserAttemptDto> userHistory = userAttemptService.getUserHistory(userAccount, technologyId)
                 .stream()
